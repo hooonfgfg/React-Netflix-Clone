6e872fce17cfdd5f45d750101269d520f18ff56e
hooonfgfg <ghdzzk@naver.com>
2022-03-23 21:50:53 +0900

    update files

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://api.themoviedb.org/3",
+  baseURL: "https://api.themoviedb.org/3",
   params: {
     api_key: "1465240c75d3440e4b45d4f3f0a65f20",
     language: "ko-KR",
